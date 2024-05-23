257631f0ed7b571432cbabd9043780726601d7a0
Parker <prkr0114@gmail.com>
2024-05-23 10:17:47 -0500

    updated solution with faster runtime for 14

--- 14 Longest Common Prefix.cpp
+++ 14 Longest Common Prefix.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string commonPrefix = strs[0], temp = "";
-        int minLength;
-        for (int i = 1; i < strs.size(); i++) {
-            minLength = min(strs[i].length(), commonPrefix.length());
-            if (minLength == 0) {
-                return "";
+        string toReturn = "";
+
+        for (int j = 0; j < strs[0].size(); j++){
+            char prev = strs[0][j];
+            if (prev == NULL){
+                return toReturn;
             }
-            for (int j = 0; j < minLength; j++) {
-                if (strs[i].at(j) == commonPrefix.at(j)) {
-                    temp += strs[i].at(j);
-                    if (j == (minLength - 1)) {
-                        commonPrefix = temp;
-                        temp = "";
-                    }
+            int yes = 1;
+            for (int i = 0; i < strs.size(); i++){
+                if (strs[i][j] == prev){
                 }
-                else {
-                    commonPrefix = temp;
-                    temp = "";
-                    break;
+                else{
+                    yes = 0;
                 }
+            }
+            if (yes == 1){
+                toReturn += prev;
+            }
+            else{
+                return toReturn;
             }
         }
-
-        return commonPrefix;
+        return toReturn;
     }
 };
