9e3a42f9251216c541ade8cba58742c3bf1e57e9
Parker <prkr0114@gmail.com>
2024-05-24 15:58:26 -0500

    updated solution to bef fast and not use .find() because cheating

--- 28 Find Index of First Occurence in String.cs
+++ 28 Find Index of First Occurence in String.cs
@@ -1,22 +1,13 @@
 class Solution {
 public:
-	// first attempt was able to build my own find method
-	// this is faster than the original using built in find
+	// fastest solution not using .find()
     int strStr(string haystack, string needle) {
-        if(haystack.size()<needle.size())
-        {
-            return -1;
+		if (haystack.length() < needle.length()) { return -1; }
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substr(i, needle.length()) == needle) { 
+                return i;
+            };
         }
-		
-		// if index of needle is not equal to non-position then return index
-		// a non-position is a constant used by the compiler
-		// the npos is thrown when needle is not found, thus breaking the if
-		// and returning -1 or not found
-		
-        if((haystack).find(needle)!=string::npos)
-        {
-            return (haystack).find(needle);
-        }
-      return -1;  
+        return -1;
     }
 };
